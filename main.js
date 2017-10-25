2f4bcd4500864aa6d92b9003841a882427d68597
J5osully <j5osully@gmail.com>
2017-10-24 21:20:31 -0500

    Added arrays
    added multiple arrays and drew the bullet array

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
-function setup() {
+var playerPos = []
+var enemyPos = []
+var playerBullets = []
+var enemyBullets = []
 
+function setup() {
+    createCanvas(600, 400);
+    background(0);
+    stroke(250, 110, 110)
+    line(0, 0, width, height)
+    playerPos = [1/2 * width, height - 10]
+
+    playerBullets = createBullet(playerBullets, playerPos[0],
+                                 playerPos[1]);
+    drawBullets(playerBullets, "blue");           
 }
 
 function draw() {
-  
+
+}
+
+// Bullet functions
+
+function createBullet(bulletArr, xPos, yPos){
+    bulletArr.push([xPos, yPos])
+    return bulletArr;
+}
+
+
+function drawBullets (bulletArr, colorString){
+    push();
+    stroke(colorString);
+    strokeWeight(5);
+
+    for (let i=0; i<bulletArr.length; i++){
+        point(bulletArr[i][0], bulletArr[i][1]);
+    }
+    pop()
 }
