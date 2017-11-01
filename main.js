f4605228d3928b04cf02d0a4c984a6e7037e8e20
J5osully <j5osully@gmail.com>
2017-10-31 22:23:15 -0500

    Added shooting, moving and key board controls
    Can now shoot bullets when spacebar is pressed 
    and can move left and right. Bullets do not 
    delete themselves.

--- main.js
+++ main.js
@@ -10,14 +10,19 @@ function setup() {
     line(0, 0, width, height)
     playerPos = [1/2 * width, height - 10]
 
-    playerBullets = createBullet(playerBullets, playerPos[0],
-                                 playerPos[1]);
-    drawBullets(playerBullets, "blue");           
+    
+               
 }
 
 function draw() {
-
+    background(0);
+    getInput();
+    playerBullets = moveBullets(playerBullets, -5);
+    drawBullets(playerBullets, "blue");
+    drawPlayer();
 }
+
+//Input Functions 
 
 // Bullet functions
 
@@ -37,3 +42,40 @@ function drawBullets (bulletArr, colorString){
     }
     pop()
 }
+
+function moveBullets(bulletArr, dy){
+    for (i=0; i<bulletArr.length; i++){
+      //bulletArr[i][1] = bulletArr[i][1] + dy;
+      bulletArr[i][1] += dy;
+    }
+    return bulletArr;
+}
+
+
+//Player functions
+function getInput(){
+    var dx = 0;
+    var dy = 0;
+    var speed = 5;
+    if (keyIsDown(LEFT_ARROW)){
+        dx = dx - speed;
+    }
+    if (keyIsDown(RIGHT_ARROW)){
+        dx = dx + speed;
+    }
+    if (keyIsDown(32)){
+        playerBullets = createBullet(playerBullets, playerPos[0],
+                                 playerPos[1]);
+    }
+    movePlayer(dx, dy);
+}
+
+function movePlayer(dx, dy){
+    playerPos[0] = playerPos[0] + dx;
+    playerPos[1] = playerPos[1] + dy;
+}
+
+function drawPlayer(){
+    drawBullets([[playerPos[0], playerPos[1]]], "green")
+}
+//Enemy Functions
